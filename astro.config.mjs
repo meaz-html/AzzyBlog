2f9f638c76f4d9d4ef72598dd19f021f52e10f5a
Ahyan Zaman <az.zmn@tuta.io>
2026-04-11 22:01:27 +0100

    Changed Branding

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 
 export default defineConfig({
-  site: "https://uinuxblog.getuinux.com",
+  site: "https://blog.azzyaz.net",
   integrations: [mdx()],
   markdown: {
     shikiConfig: {
